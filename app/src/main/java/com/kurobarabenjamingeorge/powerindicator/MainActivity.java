3ba5c6387325256930ede9312511053efc64d791
George Benjamin <msdcconnect@gmail.com>
2018-07-12 14:16:12 +0100

    Activates broadcats receiver only when the app is running

--- app/src/main/java/com/kurobarabenjamingeorge/powerindicator/MainActivity.java
+++ app/src/main/java/com/kurobarabenjamingeorge/powerindicator/MainActivity.java
@@ -1,19 +1,38 @@
 package com.kurobarabenjamingeorge.powerindicator;
 
+import android.content.ComponentName;
 import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //private PackageManager mPackageManager;
+    private PackageManager mPackageManager;
+    private ComponentName mComponentName;
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //This activates the Power receiver only when the app is running
+        mPackageManager.setComponentEnabledSetting(mComponentName,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //This deactivates the power receiver when the app is no longer visible
+        mPackageManager.setComponentEnabledSetting(mComponentName,
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        mPackageManager = getPackageManager();
+        mComponentName = new ComponentName(this, PowerReceiver.class);
     }
 }
